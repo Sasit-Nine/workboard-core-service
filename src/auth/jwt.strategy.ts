8786fb451602b1a8be208c3d3b6a97d90fab4379
6710110400 SASIT SUANSADANG <155415427+Sasit-Nine@users.noreply.github.com>
2026-02-07 22:17:35 +0700

    Authentication

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from './auth-type.type';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET || 'dev-secret',
+    });
+  }
+
+  validate(payload: JwtPayload) {
+    return {
+      email: payload.email,
+      displayName: payload.displayName,
+      isActive: payload.isActive,
+    };
+  }
+}
